8f7bbb8bc69e00d4ffe071f18b1b6b4da78508b4
prathamb15 <129771010+prathamb15@users.noreply.github.com>
2025-01-02 22:00:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var con = document.querySelector("#container"); 
+var love = document.querySelector("i"); 
+
+
+con.addEventListener("dblclick",function(){
+    love.style.transform = "translate(-50%,-50%) scale(1)";  
+    love.style.opacity =0.8;
+    console.log("HHH");
+
+
+ setTimeout(function()  { 
+   
+    love.style.opacity = 0;
+    
+ },1000);
+
+ setTimeout(function()  { 
+     love.style.transform = "translate(-50%,-50%) scale(1)";
+    
+ },3000);
+});
